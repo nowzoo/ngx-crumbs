bf76f813e40829b2dbc7fa9717aad53c6db2e424
Christopher Carson <chris@nowzoo.com>
2019-01-06 10:24:49 -0300

    initial commit

---
+++ projects/ngx-crumbs/src/lib/ngx-crumbs.module.ts
@@ -0,0 +1,43 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ObserversModule } from '@angular/cdk/observers';
+
+import { NgxCrumbsComponent } from './ngx-crumbs.component';
+import { NGX_CRUMBS_CONFIG, INgxCrumbsConfig } from './shared';
+import { NgxCrumbsWindowTitleComponent } from './ngx-crumbs-window-title.component';
+import { NgxCrumbsService } from './ngx-crumbs.service';
+import { NgxCrumbDirective } from './ngx-crumb.directive';
+
+const defaultConfig: INgxCrumbsConfig = {
+  handleWindowTitle: true,
+  windowTitleSeparator: ' | '
+};
+@NgModule({
+  declarations: [
+    NgxCrumbsComponent,
+    NgxCrumbsWindowTitleComponent,
+    NgxCrumbDirective
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ObserversModule
+  ],
+  exports: [
+    NgxCrumbsComponent,
+    NgxCrumbsWindowTitleComponent,
+    NgxCrumbDirective
+  ]
+})
+export class NgxCrumbsModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: NgxCrumbsModule,
+      providers: [
+        {provide: NGX_CRUMBS_CONFIG, useValue: defaultConfig},
+        NgxCrumbsService
+      ]
+    };
+  }
+}
